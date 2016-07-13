b2e303674d05a1eb222ccf1aa6d5f9c31299c80a
Samuel Lyon <sblyon@me.com>
2016-07-13 11:38:55 -0700

    add coords methods

--- bundle.js
+++ bundle.js
@@ -211,7 +211,28 @@
 	    return (this.x === otherCoord.x)
 	      && (this.y === otherCoord.y)
 	  }
+	
+	  getAdjacentCoords(){
+	    return Coord.MOVES.map(move => {
+	      const newX = this.x + move[0];
+	      const newY = this.y + move[1];
+	      return new Coord(newX, newY);
+	    });
+	  }
 	}
+	
+	Coord.MOVES = [
+	  [0, 1],
+	  [0, -1],
+	  [1, 0],
+	  [-1, 0],
+	  [1, 1],
+	  [1, -1],
+	  [-1, 1],
+	  [-1, -1]
+	];
+	
+	module.exports = Coord;
 
 
 /***/ }
