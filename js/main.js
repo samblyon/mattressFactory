bb1a176b4bbadddafa7b17bd85ef8196551e4453
Samuel Lyon <sblyon@me.com>
2016-07-16 22:50:59 -0700

    improve level design

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const Game = require('./game.js');
 const GameView = require('./game_view.js');
+const GameConstants = require('./game_constants');
 
+// Initialize canvas and display splash
 document.addEventListener("DOMContentLoaded", ()=>{
   console.log("Hey there and welcome to Mattress Factory Escape");
   document.addEventListener("keydown", hideSplash);
 
   const canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth - 20;
-  canvas.height = window.innerHeight - 60;
+  const body = document.getElementsByTagName('body')[0];
+  canvas.width = body.offsetWidth;
+  canvas.height = window.innerHeight - 80;
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#222";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -19,8 +22,14 @@ function pass(){
   const levelSplash = document.getElementById('level-splash');
   const victoryLine = document.getElementById('level-victory');
   victoryLine.innerHTML = `YOU HAVE BEATEN LEVEL ${level}`;
-  levelSplash.style.visibility = "visible";
+
+  ['level_header', 'level_header_2'].forEach(id => {
+    let el = document.getElementById(id);
+    el.innerHTML = GameConstants.LEVELS[level][id];
+  });
+
   level += 1;
+  levelSplash.style.visibility = "visible";
   setTimeout( () => {
     document.addEventListener("keydown", hideSplash)
   }, 2000);
